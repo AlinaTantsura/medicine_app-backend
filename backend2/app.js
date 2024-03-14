63505fd8cb5b37a42bf00bd1439be8200a628e0b
Alina <alinatanst@gmail.com>
2024-03-14 10:23:53 +0200

    Test versel

--- backend2/app.js
+++ backend2/app.js
@@ -1,21 +1,61 @@
+// import express from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import "dotenv/config.js";
+// import { medicineRouter } from "./routes/medicineRouter.js";
+// import { orderRouter } from "./routes/orderRouter.js";
+
+// const app = express();
+// const { DB_HOST, PORT } = process.env;
+
+// app.use(cors())
+// app.use(express.json());
+
+// app.use("/api/drugshops", medicineRouter);
+// app.use("/api/orders", orderRouter);
+// app.use('_', (_, res) => {
+//     res.status(404).json({
+//         message: "Route not found"
+//     })
+// });
+
+// app.use((err, req, res, next) => {
+//     const { status=500, message="Server error"} = err;
+//     res.status(status).json({ message });
+// })
+
+
+// mongoose.connect(DB_HOST).then(() => {
+//     console.log("Database connection succesful")
+//     app.listen(PORT || 3001, () => {
+//         console.log(`Server is running on port ${PORT}`)
+//     });
+// }).catch(err => {
+//     console.log(err.message);
+//     process.exit(1);
+// })
+
+
+
+
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
+
 import "dotenv/config.js";
-import { medicineRouter } from "./routes/medicineRouter.js";
-import { orderRouter } from "./routes/orderRouter.js";
 
 const app = express();
-const { DB_HOST, PORT } = process.env;
+const {PORT } = process.env;
 
 app.use(cors())
 app.use(express.json());
 
-app.use("/api/test", (req, res) => {
+app.get("/api/test", (req, res) => {
     res.json({"message": "This is the test"})
 })
-app.use("/api/drugshops", medicineRouter);
-app.use("/api/orders", orderRouter);
+
+app.get("/api/test2", (req, res) => {
+    res.json({"message": "This is the test2"})
+})
 app.use('_', (_, res) => {
     res.status(404).json({
         message: "Route not found"
@@ -27,13 +67,6 @@ app.use((err, req, res, next) => {
     res.status(status).json({ message });
 })
 
-
-mongoose.connect(DB_HOST).then(() => {
-    console.log("Database connection succesful")
     app.listen(PORT || 3001, () => {
         console.log(`Server is running on port ${PORT}`)
     });
-}).catch(err => {
-    console.log(err.message);
-    process.exit(1);
-})
